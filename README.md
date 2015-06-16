0d4ed82a2b15ebc791ee434863c88e37be2069dd
Farhad Ghayour <f@famo.us>
2015-06-15 22:05:57 -0700

    + first commit

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
-WebGL Lights
-============
+#WebGL Shadows
+> Shadow mapping in WebGL using an off-screen frame buffer
 
-Basic lights using the Phong reflection model in WebGL with phong shading.
+---
 
-### Usage
+###Demo
 
-Serve or open up `index.html`
+Here's a simple <a href="###" target="_blank">DEMO</a> showcasing an implementation of WebGL shadows using an off screen frame buffer with bit shifted depth values.
+
+Go ahead and give it a try: <a href="https://api-te.famo.us/codemanager/v1/containers/96b9e1c9-668a-4ed2-80a2-eb22bb0a3a36/share" target="_blank">DEMO</a>
+
+---
+
+###Installation
+
+```bash
+git clone https://github.com/FarhadG/webgl-shadows.git
+cd webgl-shadows
+```
+`Open` or `serve` up the `index.html` file. All of the logic happens inside of `main.js`.
+
+---
+
+###LICENSE
+
+MIT
--- main.js
+++ main.js
@@ -1,214 +1,266 @@
-// Vertex shader program
-var VSHADER_SOURCE = [
-  'attribute vec4 a_Position;',
-  'attribute vec4 a_Color;',
-  'attribute vec4 a_Normal;',
-  'uniform mat4 u_MvpMatrix;',
-  'uniform mat4 u_ModelMatrix;',
-  'uniform mat4 u_NormalMatrix;',
-  'varying vec4 v_Color;',
-  'varying vec3 v_Normal;',
-  'varying vec3 v_Position;',
-  'void main() {',
-    'gl_Position = u_MvpMatrix * a_Position;',
-     // Calculate the vertex position in the world coordinate
-    'v_Position = vec3(u_ModelMatrix * a_Position);',
-    'v_Normal = normalize(vec3(u_NormalMatrix * a_Normal));',
-    'v_Color = a_Color;',
-  '}'
-].join('')
-
-// Fragment shader program
-var FSHADER_SOURCE = [
-  'precision mediump float;',
-  'uniform vec3 u_LightColor;',
-  'uniform vec3 u_LightPosition;',
-  'uniform vec3 u_AmbientLight;',
-  'varying vec3 v_Normal;',
-  'varying vec3 v_Position;',
-  'varying vec4 v_Color;',
-  'void main() {',
-     // Normalize the normal because it is interpolated and not 1.0 in length any more
-    'vec3 normal = normalize(v_Normal);',
-     // Calculate the light direction and make its length 1.
-    'vec3 lightDirection = normalize(u_LightPosition - v_Position);',
-     // The dot product of the light direction and the orientation of a surface (the normal)
-    'float nDotL = max(dot(lightDirection, normal), 0.0);',
-     // Calculate the final color from diffuse reflection and ambient reflection
-    'vec3 diffuse = u_LightColor * v_Color.rgb * nDotL;',
-    'vec3 ambient = u_AmbientLight * v_Color.rgb;',
-    'gl_FragColor = vec4(diffuse + ambient, v_Color.a);',
-  '}'
+var SHADOW_VSHADER_SOURCE = [
+    'attribute vec4 a_Position;',
+    'uniform mat4 u_MvpMatrix;',
+    'void main() {',
+        'gl_Position = u_MvpMatrix * a_Position;',
+    '}'
 ].join('');
 
+var SHADOW_FSHADER_SOURCE = [
+    'precision mediump float;',
+    'void main() {',
+        'const vec4 bitShift = vec4(1.0, 256.0, 256.0 * 256.0, 256.0 * 256.0 * 256.0);',
+        'const vec4 bitMask = vec4(1.0/256.0, 1.0/256.0, 1.0/256.0, 0.0);',
+        'vec4 rgbaDepth = fract(gl_FragCoord.z * bitShift);',
+        'rgbaDepth -= rgbaDepth.gbaa * bitMask;',
+        'gl_FragColor = rgbaDepth;',
+    '}'
+].join('');
+
+var VSHADER_SOURCE = [
+    'attribute vec4 a_Position;',
+    'uniform mat4 u_MvpMatrix;',
+    'uniform mat4 u_MvpMatrixFromLight;',
+    'varying vec4 v_PositionFromLight;',
+    'void main() {',
+        'gl_Position = u_MvpMatrix * a_Position;',
+        'v_PositionFromLight = u_MvpMatrixFromLight * a_Position;',
+    '}'
+].join('');
+
+var FSHADER_SOURCE = [
+    'precision highp float;',
+    'uniform sampler2D u_ShadowMap;',
+    'varying vec4 v_PositionFromLight;',
+    'float unpackDepth(const in vec4 rgbaDepth) {',
+        'const vec4 bitShift = vec4(1.0, 1.0/256.0, 1.0/(256.0 * 256.0), 1.0/(256.0*256.0*256.0));',
+        'float depth = dot(rgbaDepth, bitShift);',
+        'return depth;',
+    '}',
+    'void main() {',
+        'vec3 shadowCoord = (v_PositionFromLight.xyz/v_PositionFromLight.w)/2.0 + 0.5;',
+        'vec4 rgbaDepth = texture2D(u_ShadowMap, shadowCoord.xy);',
+        'float depth = unpackDepth(rgbaDepth);',
+        'float visibility = (shadowCoord.z > depth + 0.0015) ? 0.7 : 1.0;',
+        'gl_FragColor = vec4(vec3(1.0, 0.0, 0.0) * visibility, 1.0);',
+    '}'
+].join('');
+
+var OFFSCREEN_WIDTH = 2048;
+var OFFSCREEN_HEIGHT = 2048;
+var LIGHT = [0, 7, 2];
+
 function main() {
-  // Retrieve <canvas> element
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  var canvas = document.getElementById('webgl');
-  canvas.width = width;
-  canvas.height = height;
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var canvas = document.getElementById('webgl');
+    canvas.width = width;
+    canvas.height = height;
 
-  // Get the rendering context for WebGL
-  var gl = getWebGLContext(canvas);
-  if (!gl) {
-    console.log('Failed to get the rendering context for WebGL');
-    return;
-  }
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.error('Failed to get the rendering context for WebGL');
+        return;
+    }
 
-  // Initialize shaders
-  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
-    console.log('Failed to intialize shaders.');
-    return;
-  }
+    var shadowProgram = createProgram(gl, SHADOW_VSHADER_SOURCE, SHADOW_FSHADER_SOURCE);
+    shadowProgram.a_Position = gl.getAttribLocation(shadowProgram, 'a_Position');
+    shadowProgram.u_MvpMatrix = gl.getUniformLocation(shadowProgram, 'u_MvpMatrix');
 
-  var n = initVertexBuffers(gl);
-  if (n < 0) {
-    console.log('Failed to set the vertex information');
-    return;
-  }
+    var normalProgram = createProgram(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+    normalProgram.a_Position = gl.getAttribLocation(normalProgram, 'a_Position');
+    normalProgram.u_MvpMatrix = gl.getUniformLocation(normalProgram, 'u_MvpMatrix');
+    normalProgram.u_MvpMatrixFromLight = gl.getUniformLocation(normalProgram, 'u_MvpMatrixFromLight');
+    normalProgram.u_ShadowMap = gl.getUniformLocation(normalProgram, 'u_ShadowMap');
 
-  // Set the clear color and enable the depth test
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.enable(gl.DEPTH_TEST);
+    var triangle = initVertexBuffersForTriangle(gl);
+    var sphere = initVertexBuffersForSphere(gl);
 
-  // Get the storage locations of uniform variables
-  var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
-  var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
-  var u_NormalMatrix = gl.getUniformLocation(gl.program, 'u_NormalMatrix');
-  var u_LightColor = gl.getUniformLocation(gl.program, 'u_LightColor');
-  var u_LightPosition = gl.getUniformLocation(gl.program, 'u_LightPosition');
-  var u_AmbientLight = gl.getUniformLocation(gl.program, 'u_AmbientLight');
-  if (!u_ModelMatrix || !u_MvpMatrix || !u_NormalMatrix || !u_LightColor || !u_LightPosition　|| !u_AmbientLight) {
-    console.log('Failed to get the storage location');
-    return;
-  }
+    var fbo = initFramebufferObject(gl);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, fbo.texture);
 
-  // Set the light color (white)
-  gl.uniform3f(u_LightColor, 1.0, 1.0, 1.0);
-  // Set the light direction (in the world coordinate)
-  gl.uniform3f(u_LightPosition, 2.3, 4.0, 3.5);
-  // Set the ambient light
-  gl.uniform3f(u_AmbientLight, 0.2, 0.2, 0.2);
+    gl.clearColor(0, 0, 0, 1);
+    gl.enable(gl.DEPTH_TEST);
 
-  var modelMatrix = new Matrix4();  // Model matrix
-  var mvpMatrix = new Matrix4();    // Model view projection matrix
-  var normalMatrix = new Matrix4(); // Transformation matrix for normals
+    var viewProjMatrixFromLight = new Matrix4();
+    viewProjMatrixFromLight.setPerspective(70.0, OFFSCREEN_WIDTH/OFFSCREEN_HEIGHT, 1.0, 200.0);
+    viewProjMatrixFromLight.lookAt(LIGHT[0], LIGHT[1], LIGHT[2], 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-  // Calculate the model matrix
-  modelMatrix.setRotate(90, 0, 1, 0); // Rotate around the y-axis
-  // Calculate the view projection matrix
-  mvpMatrix.setPerspective(30, canvas.width/canvas.height, 1, 100);
-  mvpMatrix.lookAt(6, 6, 14, 0, 0, 0, 0, 1, 0);
-  mvpMatrix.multiply(modelMatrix);
-  // Calculate the matrix to transform the normal based on the model matrix
-  normalMatrix.setInverseOf(modelMatrix);
-  normalMatrix.transpose();
+    var viewProjMatrix = new Matrix4();
+    viewProjMatrix.setPerspective(45, canvas.width/canvas.height, 1.0, 100.0);
+    viewProjMatrix.lookAt(0.0, 7.0, 9.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-  // Pass the model matrix to u_ModelMatrix
-  gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+    var currentAngle = 0.0;
+    var mvpMatrixFromLight_t = new Matrix4();
+    var mvpMatrixFromLight_p = new Matrix4();
 
-  // Pass the model view projection matrix to u_mvpMatrix
-  gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+    var tick = function() {
+        currentAngle = animate(currentAngle);
 
-  // Pass the transformation matrix for normals to u_NormalMatrix
-  gl.uniformMatrix4fv(u_NormalMatrix, false, normalMatrix.elements);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+        gl.viewport(0, 0, OFFSCREEN_HEIGHT, OFFSCREEN_HEIGHT);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  // Clear color and depth buffer
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        gl.useProgram(shadowProgram);
+        drawTriangle(gl, shadowProgram, triangle, currentAngle, viewProjMatrixFromLight);
+        mvpMatrixFromLight_t.set(g_mvpMatrix);
+        drawSphere(gl, shadowProgram, sphere, viewProjMatrixFromLight);
+        mvpMatrixFromLight_p.set(g_mvpMatrix);
 
-  // Draw the cube
-  gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        gl.viewport(0, 0, canvas.width, canvas.height);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        gl.useProgram(normalProgram);
+        gl.uniform1i(normalProgram.u_ShadowMap, 0);
+
+        gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight, false, mvpMatrixFromLight_t.elements);
+        drawTriangle(gl, normalProgram, triangle, currentAngle, viewProjMatrix);
+        gl.uniformMatrix4fv(normalProgram.u_MvpMatrixFromLight, false, mvpMatrixFromLight_p.elements);
+        drawSphere(gl, normalProgram, sphere, viewProjMatrix);
+
+        window.requestAnimationFrame(tick, canvas);
+    };
+    tick();
 }
 
-function initVertexBuffers(gl) {
-  // Create a cube
-  //    v6----- v5
-  //   /|      /|
-  //  v1------v0|
-  //  | |     | |
-  //  | |v7---|-|v4
-  //  |/      |/
-  //  v2------v3
-  // Coordinates
-  var vertices = new Float32Array([
-     2.0, 2.0, 2.0,  -2.0, 2.0, 2.0,  -2.0,-2.0, 2.0,   2.0,-2.0, 2.0, // v0-v1-v2-v3 front
-     2.0, 2.0, 2.0,   2.0,-2.0, 2.0,   2.0,-2.0,-2.0,   2.0, 2.0,-2.0, // v0-v3-v4-v5 right
-     2.0, 2.0, 2.0,   2.0, 2.0,-2.0,  -2.0, 2.0,-2.0,  -2.0, 2.0, 2.0, // v0-v5-v6-v1 up
-    -2.0, 2.0, 2.0,  -2.0, 2.0,-2.0,  -2.0,-2.0,-2.0,  -2.0,-2.0, 2.0, // v1-v6-v7-v2 left
-    -2.0,-2.0,-2.0,   2.0,-2.0,-2.0,   2.0,-2.0, 2.0,  -2.0,-2.0, 2.0, // v7-v4-v3-v2 down
-     2.0,-2.0,-2.0,  -2.0,-2.0,-2.0,  -2.0, 2.0,-2.0,   2.0, 2.0,-2.0  // v4-v7-v6-v5 back
-  ]);
-
-  // Colors
-  var colors = new Float32Array([
-    0.5, 1, 0,   0.5, 1, 0,   0.5, 1, 0,  0.5, 1, 0,     // v0-v1-v2-v3 front
-    0.5, 1, 0,   0.5, 1, 0,   0.5, 1, 0,  0.5, 1, 0,     // v0-v3-v4-v5 right
-    0.5, 1, 0,   0.5, 1, 0,   0.5, 1, 0,  0.5, 1, 0,     // v0-v5-v6-v1 up
-    0.5, 1, 0,   0.5, 1, 0,   0.5, 1, 0,  0.5, 1, 0,     // v1-v6-v7-v2 left
-    0.5, 1, 0,   0.5, 1, 0,   0.5, 1, 0,  0.5, 1, 0,     // v7-v4-v3-v2 down
-    0.5, 1, 0,   0.5, 1, 0,   0.5, 1, 0,  0,5, 1, 0　    // v4-v7-v6-v5 back
- ]);
-
-  // Normal
-  var normals = new Float32Array([
-    0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,  // v0-v1-v2-v3 front
-    1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,  // v0-v3-v4-v5 right
-    0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,  // v0-v5-v6-v1 up
-   -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  // v1-v6-v7-v2 left
-    0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,  // v7-v4-v3-v2 down
-    0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0   // v4-v7-v6-v5 back
-  ]);
-
-  // Indices of the vertices
-  var indices = new Uint8Array([
-     0, 1, 2,   0, 2, 3,    // front
-     4, 5, 6,   4, 6, 7,    // right
-     8, 9,10,   8,10,11,    // up
-    12,13,14,  12,14,15,    // left
-    16,17,18,  16,18,19,    // down
-    20,21,22,  20,22,23     // back
- ]);
-
-  // Write the vertex property to buffers (coordinates, colors and normals)
-  if (!initArrayBuffer(gl, 'a_Position', vertices, 3)) return -1;
-  if (!initArrayBuffer(gl, 'a_Color', colors, 3)) return -1;
-  if (!initArrayBuffer(gl, 'a_Normal', normals, 3)) return -1;
-
-  // Unbind the buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-  // Write the indices to the buffer object
-  var indexBuffer = gl.createBuffer();
-  if (!indexBuffer) {
-    console.log('Failed to create the buffer object');
-    return false;
-  }
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
-  return indices.length;
+var g_modelMatrix = new Matrix4();
+var g_mvpMatrix = new Matrix4();
+function drawTriangle(gl, program, triangle, angle, viewProjMatrix) {
+    g_modelMatrix.setRotate(angle, 0, 1, 0);
+    draw(gl, program, triangle, viewProjMatrix);
 }
 
-function initArrayBuffer(gl, attribute, data, num) {
-  // Create a buffer object
-  var buffer = gl.createBuffer();
-  if (!buffer) {
-    console.log('Failed to create the buffer object');
-    return false;
-  }
-  // Write date into the buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-  // Assign the buffer object to the attribute variable
-  var a_attribute = gl.getAttribLocation(gl.program, attribute);
-  if (a_attribute < 0) {
-    console.log('Failed to get the storage location of ' + attribute);
-    return false;
-  }
-  gl.vertexAttribPointer(a_attribute, num, gl.FLOAT, false, 0, 0);
-  // Enable the assignment of the buffer object to the attribute variable
-  gl.enableVertexAttribArray(a_attribute);
+function drawSphere(gl, program, sphere, viewProjMatrix) {
+    g_modelMatrix.setScale(4.0, 4.0, 4.0);
+    g_modelMatrix.translate(0.0, -2.0, 0.0);
+    draw(gl, program, sphere, viewProjMatrix);
+}
 
-  return true;
+function draw(gl, program, o, viewProjMatrix) {
+    initAttributeVariable(gl, program.a_Position, o.vertexBuffer);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, o.indexBuffer);
+    g_mvpMatrix.set(viewProjMatrix);
+    g_mvpMatrix.multiply(g_modelMatrix);
+    gl.uniformMatrix4fv(program.u_MvpMatrix, false, g_mvpMatrix.elements);
+    gl.drawElements(gl.TRIANGLES, o.numIndices, gl.UNSIGNED_BYTE, 0);
+}
+
+function initAttributeVariable(gl, a_attribute, buffer) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.vertexAttribPointer(a_attribute, buffer.num, buffer.type, false, 0, 0);
+    gl.enableVertexAttribArray(a_attribute);
+}
+
+function initVertexBuffersForTriangle(gl) {
+    var vertices = new Float32Array([
+        -0.8, 3.5, 0.0,
+         0.8, 3.5, 0.0,
+         0.0, 3.5, 1.8
+    ]);
+    var indices = new Uint8Array([0, 1, 2]);
+    return {
+        vertexBuffer: initArrayBufferForLaterUse(gl, vertices, 3, gl.FLOAT),
+        indexBuffer: initElementArrayBufferForLaterUse(gl, indices, gl.UNSIGNED_BYTE),
+        numIndices: indices.length
+    };
+}
+
+function initVertexBuffersForSphere(gl) {
+    var SPHERE_DIV = 20;
+    var i, ai, si, ci,
+        j, aj, sj, cj,
+        p1, p2;
+
+    var vertices = [];
+    var indices = [];
+
+    for(j = 0; j <= SPHERE_DIV; j++) {
+        aj = j * Math.PI / SPHERE_DIV;
+        sj = Math.sin(aj);
+        cj = Math.cos(aj);
+        for(i = 0; i <= SPHERE_DIV; i++) {
+            ai = i * 2 * Math.PI / SPHERE_DIV;
+            si = Math.sin(ai);
+            ci = Math.cos(ai);
+            vertices.push(si * sj);
+            vertices.push(cj);
+            vertices.push(ci * sj);
+        }
+    }
+
+    for(j = 0; j < SPHERE_DIV; j++) {
+        for(i = 0; i < SPHERE_DIV; i++) {
+            p1 = j * (SPHERE_DIV+1) + i;
+            p2 = p1 + (SPHERE_DIV+1);
+
+            indices.push(p1);
+            indices.push(p2);
+            indices.push(p1 + 1);
+
+            indices.push(p1 + 1);
+            indices.push(p2);
+            indices.push(p2 + 1);
+        }
+    }
+    return {
+        vertexBuffer: initArrayBufferForLaterUse(gl, new Float32Array(vertices), 3, gl.FLOAT),
+        indexBuffer: initElementArrayBufferForLaterUse(gl, new Uint8Array(indices), gl.UNSIGNED_BYTE),
+        numIndices: indices.length
+    };
+}
+
+function initArrayBufferForLaterUse(gl, data, num, type) {
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+    buffer.num = num;
+    buffer.type = type;
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    return buffer;
+}
+
+function initElementArrayBufferForLaterUse(gl, indices, type) {
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+    buffer.type = type;
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    return buffer;
+}
+
+function initFramebufferObject(gl) {
+    var texture, depthBuffer;
+
+    var framebuffer = gl.createFramebuffer();
+
+    texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, OFFSCREEN_WIDTH, OFFSCREEN_HEIGHT, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    depthBuffer = gl.createRenderbuffer();
+    gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer);
+    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, OFFSCREEN_WIDTH, OFFSCREEN_HEIGHT);
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthBuffer);
+
+    framebuffer.texture = texture;
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+    return framebuffer;
+}
+
+var ANGLE_STEP = 40;
+var last = Date.now();
+function animate(angle) {
+    var now = Date.now();
+    var elapsed = now - last;
+    last = now;
+    var newAngle = angle + (ANGLE_STEP * elapsed) / 1000.0;
+    return newAngle % 360;
 }
